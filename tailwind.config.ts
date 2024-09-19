7840affc6a60049dfc62735cb10da1a2968e628b
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-09-20 02:30:47 +0600

    create navber

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,27 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        customGray: '#363636',
       },
+      height: {
+        '101': '101px',
+        '87': '87px',
+        '44': '44px',
+      },
+      width: {
+        '1253': '1253px',
+        "20": "20px",
+        "298": "298px",
+        "235": "235px"
+      },
+      margin: {
+        '88': '88px',
+        "20": "20px",
+        "44": "44px",
+      },
+      padding: {
+        "20": "20px"
+      }
     },
   },
   plugins: [],
